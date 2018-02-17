054e4c6babc4a8e897f01a51e25f474072588bdb
zlydenko <anton.zlydenko@gmail.com>
2018-02-17 22:14:56 +0200

    feat: made bestprice tablet/desktop version

--- src/BestPrice/index.js
+++ src/BestPrice/index.js
@@ -8,10 +8,6 @@ import ruFlag2x from "./flags/ru2x.png";
 import amFlag2x from "./flags/am2x.png";
 import mdFlag2x from "./flags/md2x.png";
 
-const Container = styled.section`
-  background: linear-gradient(180deg, #f8fcff 0%, #ffffff 100%);
-`;
-
 const CalendarIcon = styled.div`
   width: 64px;
   height: 64px;
@@ -25,7 +21,7 @@ const CalendarIcon = styled.div`
   margin-bottom: 20px;
 `;
 
-const Title = styled.h1`
+const Title = styled.h2`
   margin-right: auto;
   margin-left: auto;
   margin-bottom: 40px;
@@ -36,12 +32,16 @@ const Title = styled.h1`
   color: #4a4a4a;
 `;
 
-const DestinationTitle = styled.h2`
+const DestinationTitle = styled.p`
   font-size: 22px;
   line-height: 32px;
   color: #5b5b5c;
   margin: 0px;
   margin-bottom: 2px;
+
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 const Country = styled.p`
@@ -51,6 +51,7 @@ const Country = styled.p`
   font-size: 12px;
   line-height: 20px;
   color: #a0b0b9;
+  text-align: left;
 `;
 
 const Flag = styled.div`
@@ -67,18 +68,28 @@ const Price = styled.span`
   font-size: 16px;
   line-height: 20px;
   color: #00bae8;
+  text-align: right;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 const DepartureFrom = styled.span`
   font-size: 16px;
   line-height: 20px;
   color: #4a4a4a;
+  text-align: left;
+
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 const Header = styled.header`
   display: flex;
   align-items: center;
   margin-bottom: 24px;
+  flex-wrap: no-wrap;
 `;
 
 const Row = styled.div`
@@ -94,8 +105,9 @@ const Row = styled.div`
 const Card = styled.div`
   margin-bottom: 24px;
   border-bottom: 2px dashed #afbec6;
-  &:last-child {
-    border-bottom: none;
+
+  @media (min-width: 1000px) {
+    border: none;
   }
 `;
 
@@ -207,21 +219,18 @@ const ChinesauCard = () => (
   </Card>
 );
 
-const Main = styled.main`
-  padding-bottom: 40px;
-`;
-
-const Footer = styled.footer`
-  padding-bottom: 40px;
-`;
-
 const Text = styled.p`
   font-size: 16px;
-  line-height: 24px;
+  line-height: 26px;
   text-align: center;
   color: #4a4a4a;
   margin: 0px;
   margin-bottom: 24px;
+  margin-top: 40px;
+
+  @media (min-width: 1200px) {
+    margin-top: 80px;
+  }
 `;
 
 const Caption = styled.p`
@@ -230,25 +239,38 @@ const Caption = styled.p`
   text-align: center;
   color: #a0b0b9;
   margin: 0px;
+  margin-bottom: 40px;
   font-weight: 300;
+
+  @media (min-width: 768px) {
+    margin-bottom: 84px;
+  }
+
+  @media (min-width: 1200px) {
+    margin-bottom: 75px;
+  }
 `;
 
 const BestPrice = () => (
-  <Container>
-    <div className="container">
-      <CalendarIcon>
-        <img src={calendar} alt="" />
-      </CalendarIcon>
+  <div className="container">
+    <CalendarIcon>
+      <img src={calendar} alt="" />
+    </CalendarIcon>
 
-      <Title>Лучшие цены на авиабилеты за последний месяц</Title>
+    <Title>Лучшие цены на авиабилеты за последний месяц</Title>
 
-      <Main>
+    <div className="row center-xs">
+      <div className="col-xs-12 col-md-10 col-lg-4">
         <SimferopolCard />
+      </div>
+      <div className="col-xs-12 col-md-10 col-lg-4">
         <ErevanCard />
+      </div>
+      <div className="col-xs-12 col-md-10 col-lg-4">
         <ChinesauCard />
-      </Main>
+      </div>
 
-      <Footer>
+      <div className="col-xs-12 col-md-10 col-lg-6">
         <Text>
           Мы знаем, где купить авиабилеты дешево. Билеты на самолет в 220 стран
           мира. Поиск и сравнение цен на авиабилеты среди 100 агентств и 728
@@ -258,9 +280,9 @@ const BestPrice = () => (
           Цены, найденные пользователями за последние 48 часов, не являются
           офертой.
         </Caption>
-      </Footer>
+      </div>
     </div>
-  </Container>
+  </div>
 );
 
 export default BestPrice;
