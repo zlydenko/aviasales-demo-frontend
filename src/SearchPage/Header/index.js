295b33108ef1e2c56d7dac62b243108c67851c37
zlydenko <anton.zlydenko@gmail.com>
2018-02-22 14:28:13 +0200

    fix: add link to main page in header

--- src/SearchPage/Header/index.js
+++ src/SearchPage/Header/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import { Header, Info, Back, Title, Text, Currency } from "./styled";
+import { Link } from "react-router-dom";
 import back from "./back.svg";
 
 export default () => (
   <Header>
     <Info>
-      <Back src={back} alt="" />
+      <Link to="/">
+        <Back src={back} alt="" />
+      </Link>
       <div>
         <Title>Москва — Барселона</Title>
         <Text>24 фев — 3 март, 1 пассажир</Text>
