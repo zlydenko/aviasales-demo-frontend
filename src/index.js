8e422f8a572ea753a9d135fdf9fc21354a5f6ff4
zlydenko <anton.zlydenko@gmail.com>
2018-02-22 10:56:04 +0200

    feat: add basic routing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "normalize.css";
 import "flexboxgrid2";
 import "./index.css";
@@ -8,5 +9,14 @@ import registerServiceWorker from "./registerServiceWorker";
 import MainPage from "./MainPage";
 import Search from "./SearchPage";
 
-ReactDOM.render(<Search />, document.getElementById("root"));
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={MainPage} />
+      <Route path="/search/" component={Search} />
+    </div>
+  </Router>
+);
+
+render(<App />, document.getElementById("root"));
 registerServiceWorker();
