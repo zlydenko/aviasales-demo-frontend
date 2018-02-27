a031a6c69e0fcd1577516fedfe80670e0c35ad44
zlydenko <anton.zlydenko@gmail.com>
2018-02-28 01:26:55 +0200

    feat: raw version of sidebar for xl screens

--- src/SearchPage/Main/index.js
+++ src/SearchPage/Main/index.js
@@ -2,20 +2,27 @@ import React from "react";
 import { GoUp, Main, FilterBtn } from "./styled";
 import filter from "./filter.svg";
 import Card from "./Card";
+import Sidebar from "./Sidebar";
 import tickets from "../../data/tickets";
 
 export default () => (
   <Main>
-    <GoUp>
-      <div className="hidden-md hidden-lg hidden-xl">Наверх</div>
-      <img src={filter} alt="" />
-    </GoUp>
-    <div className="row">
-      <div className="col-xs-3">sadas</div>
-      <div className="col-xs-12 col-xl-7">
-        {tickets.map((ticket, key) => <Card key={key} data={ticket} />)}
+    <div className="container">
+      <div className="hidden-xl">
+        <GoUp>
+          <div className="hidden-md hidden-lg hidden-xl">Наверх</div>
+          <img src={filter} alt="" />
+        </GoUp>
       </div>
+      <div className="row">
+        <div className="col-xl-3">
+          <Sidebar />
+        </div>
+        <div className="col-xs-12 col-xl-7">
+          {tickets.map((ticket, key) => <Card key={key} data={ticket} />)}
+        </div>
+      </div>
+      <FilterBtn>Фильтровать</FilterBtn>
     </div>
-    <FilterBtn>Фильтровать</FilterBtn>
   </Main>
 );
