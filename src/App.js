974e65b5000597aa4a343e5241f5ca964207d0ff
zlydenko <anton.zlydenko@gmail.com>
2018-02-28 00:09:25 +0200

    feat: add locale provider from react-inlt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { IntlProvider, addLocaleData } from "react-intl";
+import ru from "react-intl/locale-data/ru";
 
 import MainPage from "./MainPage";
 import Search from "./SearchPage";
 
+addLocaleData(ru);
+
 export default () => (
-  <Router>
-    <div>
-      <Route exact path="/" component={MainPage} />
-      <Route path="/result" component={Search} />
-    </div>
-  </Router>
+  <IntlProvider locale="ru">
+    <Router>
+      <div>
+        <Route exact path="/" component={MainPage} />
+        <Route path="/result" component={Search} />
+      </div>
+    </Router>
+  </IntlProvider>
 );
