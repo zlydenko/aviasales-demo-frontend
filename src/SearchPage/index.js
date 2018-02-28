9aa901d3eca3ee724b339d43571122705ecae9a9
zlydenko <anton.zlydenko@gmail.com>
2018-02-28 10:46:23 +0200

    fix: add all sections of results page

--- src/SearchPage/index.js
+++ src/SearchPage/index.js
@@ -2,16 +2,12 @@ import React from "react";
 
 import Header from "./Header";
 import Main from "./Main";
-import Footer from "../MainPage/Footer";
+import Footer from "../Footer";
 
-export default class Search extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
-}
+export default () => (
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+);
